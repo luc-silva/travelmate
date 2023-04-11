482b2ae04f420d6023c7aadaab9f74013a35dac6
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-11 20:17:00 -0300

    Add Room class

--- src/entities/Room.java
+++ src/entities/Room.java
@@ -1,5 +1,32 @@
 package entities;
 
-public class Room {
+import entities.enums.RoomStatus;
 
+public class Room {
+    private double dailyPrice;
+    private RoomStatus status;
+    public Room(Double dailyPrice, RoomStatus status){
+        this.dailyPrice = dailyPrice;
+        this.status = status;
+    }
+    public Room(){
+        this.dailyPrice = 456.0;
+        this.status = RoomStatus.EMPTY;
+    }
+
+    public void setDailyPrice(double dailyPrice) {
+        this.dailyPrice = dailyPrice;
+    }
+
+    public double getDailyPrice() {
+        return dailyPrice;
+    }
+
+    public void setStatus(RoomStatus status) {
+        this.status = status;
+    }
+
+    public RoomStatus getStatus() {
+        return status;
+    }
 }
