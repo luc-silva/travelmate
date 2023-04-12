70196bfbf224f7a8a258e1949fa83c61c570a4d7
Lucas Santos <lucas.silva1203@outlook.com>
2023-04-12 20:27:08 -0300

    Override Bed toString method

--- src/entities/Bed.java
+++ src/entities/Bed.java
@@ -2,4 +2,12 @@ package entities;
 
 public final class Bed {
     private String type;
+    public Bed(String type){
+        this.type = type;
+    }
+    @Override
+    public String toString(){
+        return this.type;
+    }
+
 }
