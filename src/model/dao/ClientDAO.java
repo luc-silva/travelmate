81464a6ab5d166421a0e0eedbb41029f21e42dd2
Lucas Santos <lucas.silva1203@outlook.com>
2023-05-30 16:49:47 -0300

    Add new method to Client and Room DAO interfaces

--- src/model/dao/ClientDAO.java
+++ src/model/dao/ClientDAO.java
@@ -2,8 +2,11 @@ package model.dao;
 
 import model.entities.Client;
 
+import java.util.List;
+
 public interface ClientDAO {
     public Client findClientById(Integer id);
+    public List<Client> listClients();
     public void addClient(Client client);
     public void deleteClientById(Integer id);
     public void updateClientById(Integer id, Client updatedClient);
--- src/model/dao/RoomDAO.java
+++ src/model/dao/RoomDAO.java
@@ -2,10 +2,13 @@ package model.dao;
 
 import model.entities.Room;
 
+import java.util.List;
+
 public interface RoomDAO {
     public Room findRoomById(Integer id);
     public Room findRoomByNumber(Integer door_number);
     public Room findRoomByResidentId(Integer id);
+    public List<Room> listRooms();
     public void addRoom(Room client);
     public void deleteRoomById(Integer id);
     public void updateRoomById(Integer id, Room updatedRoom);
