7c4b7b7ef407b126fe43bfa5d4f06e9c935380ae
Lucas Santos <lucas.silva1203@outlook.com>
2023-05-30 17:49:55 -0300

    Add listAvalaibleRooms method to interface

--- src/model/dao/RoomDAO.java
+++ src/model/dao/RoomDAO.java
@@ -9,6 +9,7 @@ public interface RoomDAO {
     public Room findRoomByNumber(Integer door_number);
     public Room findRoomByResidentId(Integer id);
     public List<Room> listRooms();
+    public List<Room> listAvailableRooms();
     public void addRoom(Room client);
     public void deleteRoomById(Integer id);
     public void updateRoomById(Integer id, Room updatedRoom);
